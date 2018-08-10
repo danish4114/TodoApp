f2af86a729af65c4fbd8c2f1295dc01ad96a8a8d
danish4114 <mohd_danish@excellencetechnologies.in>
2018-08-10 16:48:44 +0530

    step2 of TodoApp

---
+++ src/addtodolist.js
@@ -0,0 +1,29 @@
+import React from "react";
+class Addtodolist extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="container">
+        <form>
+          <label>Todo</label>
+          <input
+            type="text"
+            placeholder="Add todo list"
+            value={this.props.fetch}
+            className="form-control"
+            onChange={this.props.target}
+          />
+          <button
+            className="btn btn-normal"
+            onClick={this.props.submit}
+            type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+export default Addtodolist;
